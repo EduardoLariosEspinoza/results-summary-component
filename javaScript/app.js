845351a4c2c4b158421c24f49b053cd513817a8b
Eduardo Larios Espinoza <elariosespinoza2@gmail.com>
2024-02-22 16:17:55 -0800

    first commit

---
+++ javaScript/app.js
@@ -0,0 +1,16 @@
+const entero = 10;
+const flotante = 10.5;
+const string = "Hola Mundo";
+const boolean = true;
+const array = [1, 2, 3, 4, 5];
+const object = {
+  elemento: "valor",
+  elemento2: "valor2",
+};
+
+console.log("Entero:", entero);
+console.log("Flotante:", flotante);
+console.log("String:", string);
+console.log("Boolean:", boolean);
+console.log("Array:", array);
+console.log("Object:", object);
